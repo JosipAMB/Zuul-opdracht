dacfca7c2446b81ced57068b73474baefa5f2c6c
JosipAMB <j.ambrozic@student.alfa-college.nl>
2026-01-19 12:24:27 +0200

    Update Player.cs

--- Zuul/Zuul/src/Player.cs
+++ Zuul/Zuul/src/Player.cs
@@ -2,18 +2,44 @@ using System;
 
 class Player
 {
-    // auto property
-    public Room CurrentRoom { get; set; }
-
-    // fields
-    private int health;
-
+    // field
+    private Inventory backpack;
     // constructor
     public Player()
     {
-        CurrentRoom = null;
-        health = 100;
+    // 25kg is best zwaar om de hele dag te dragen
+    backpack = new Inventory(25);
+    CurrentRoom = null;
+    health = 100;
     }
+    // methods
+    public bool TakeFromChest(string itemName)
+    {
+    // TODO implementeer:
+    // Haal het Item uit de Room
+    // Zet het in je backpack
+    // Bekijk de return values
+    // Past het Item niet? Zet het terug in de chest
+    // Laat de speler weten wat er gebeurt
+    // Return true/false voor succes/mislukt
+    return false;
+    }
+    public bool DropToChest(string itemName)
+    {
+    // TODO implementeer:
+    // Haal Item uit je backpack
+    // Zet het in de Room
+    // Bekijk de return values
+    // Laat de speler weten wat er gebeurt
+    // Return true/false voor succes/mislukt
+    return false;
+    }
+
+    // auto property
+    public Room CurrentRoom { get; set; }
+    // fields
+    private int health;
+    // constructor
 
     // methods
     public void Damage(int amount) // speler verliest health
@@ -21,18 +47,15 @@ class Player
         health -= amount;
         if (health < 0) health = 0;
     }
-
     public void Heal(int amount) // speler krijgt health
     {
         health += amount;
-        if (health > 100) health = 100; // assuming max health is 100
+        if (health > 100) health = 100; // max health is 100
     }
-
     public bool IsAlive() // checkt of speler nog leeft
     {
         return health > 0;
     }
-
     public int Health
     {
         get { return health; }
