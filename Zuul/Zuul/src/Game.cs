be82785a8e0f97cf018497c8de08af09d252bdca
JosipAMB <j.ambrozic@student.alfa-college.nl>
2026-01-19 12:53:16 +0200

    Update Game.cs

--- Zuul/Zuul/src/Game.cs
+++ Zuul/Zuul/src/Game.cs
@@ -10,6 +10,32 @@ class Game
 		player = new Player();
 		CreateRooms();
 	}
+	// methods
+// methods
+private void Take(Command command)
+{
+    if (!command.HasSecondWord())
+    {
+        Console.WriteLine("Take what?");
+        return;
+    }
+
+    string itemName = command.SecondWord;
+    player.TakeFromChest(itemName);
+}
+
+private void Drop(Command command)
+{
+    if (!command.HasSecondWord())
+    {
+        Console.WriteLine("Drop what?");
+        return;
+    }
+
+    string itemName = command.SecondWord;
+    player.DropToChest(itemName);
+}
+
 
 	// Initialise the Rooms (and the Items)
 		private void CreateRooms()
@@ -19,12 +45,10 @@ class Game
 		Room pub = new Room("in the campus pub");
 		Room lab = new Room("in a computing lab");
 		Room office = new Room("in the computing admin office");
-
 		// Extra kamers voor verdiepingen
 		Room labUp = new Room("in the upper floor of the computing lab");
 		Room officeUp = new Room("in the upper floor of the admin office");
 
-		// Stel de uitgangen in
 		outside.AddExit("east", theatre);
 		outside.AddExit("south", lab);
 		outside.AddExit("west", pub);
@@ -40,16 +64,21 @@ class Game
 		office.AddExit("west", lab);
 		office.AddExit("up", officeUp);    // naar boven
 		officeUp.AddExit("down", office);  // naar beneden
-
+		// Maak items
+		Item sword = new Item(5, "sword");
+		Item book = new Item(2, "book");
+		Item key = new Item(1, "key");
+		// Voeg items toe aan kamers
+		outside.AddItem(book);
+		lab.AddItem(sword);
+		officeUp.AddItem(key);
 		// Start buiten
 		player.CurrentRoom = outside;
 	}
-
 	//  Main play routine. Loops until end of play.
 	public void Play()
 	{
 		PrintWelcome();
-
 		// Enter the main command loop. Here we repeatedly read commands and
 		// execute them until the player wants to quit.
 		bool finished = false;
@@ -62,7 +91,6 @@ class Game
 		Console.WriteLine("Press [Enter] to continue.");
 		Console.ReadLine();
 	}
-
 	// Print out the opening message for the player.
 	private void PrintWelcome()
 	{
@@ -73,7 +101,6 @@ class Game
 		Console.WriteLine();
 		Console.WriteLine(player.CurrentRoom.GetLongDescription());
 	}
-
 	// Given a command, process (that is: execute) the command.
 	// If this command ends the game, it returns true.
 	// Otherwise false is returned.
@@ -92,7 +119,6 @@ class Game
     		Console.WriteLine(player.CurrentRoom.GetLongDescription());
 		}
 
-
 		switch (command.CommandWord)
 		{
 			case "help":
@@ -101,14 +127,18 @@ class Game
 			case "go":
 				GoRoom(command);
 				break;
+			case "take":
+				Take(command);
+				break;
+			case "drop":
+				Drop(command);
+				break;
 			case "quit":
 				wantToQuit = true;
 				break;
 		}
-
 		return wantToQuit;
 	}
-
 	// ######################################
 	// implementations of user commands:
 	// ######################################
@@ -126,43 +156,43 @@ class Game
 
 	// Try to go to one direction. If there is an exit, enter the new
 	// room, otherwise print an error message.
-private void GoRoom(Command command)
-{
-    if (!command.HasSecondWord())
-    {
-        Console.WriteLine("Go where?");
-        return;
-    }
+	private void GoRoom(Command command)
+	{
+		if (!command.HasSecondWord())
+		{
+			Console.WriteLine("Go where?");
+			return;
+		}
 
-    string direction = command.SecondWord;
-    Room nextRoom = player.CurrentRoom.GetExit(direction);
+		string direction = command.SecondWord;
+		Room nextRoom = player.CurrentRoom.GetExit(direction);
 
-    if (nextRoom == null)
-    {
-        Console.WriteLine("There is no door to " + direction + "!");
-        return;
-    }
+		if (nextRoom == null)
+		{
+			Console.WriteLine("There is no door to " + direction + "!");
+			return;
+		}
 
-    // beweeg speler
-    player.CurrentRoom = nextRoom;
+		// beweeg speler
+		player.CurrentRoom = nextRoom;
 
-    // verlies health
-    player.Damage(5);
-    Console.WriteLine(player.CurrentRoom.GetLongDescription());
-    Console.WriteLine("Your health: " + player.Health);
+		// verlies health
+		player.Damage(5);
+		Console.WriteLine(player.CurrentRoom.GetLongDescription());
+		Console.WriteLine("Your health: " + player.Health);
 
-    // check dood
-    if (!player.IsAlive())
-    {
-        Console.WriteLine("You have died! Game over.");
-        Environment.Exit(0);
-    }
+		// check dood
+		if (!player.IsAlive())
+		{
+			Console.WriteLine("You have died! Game over.");
+			Environment.Exit(0);
+		}
 
-    // check win: gebruik de description van de kamer
-    if (player.CurrentRoom.GetShortDescription() == "in the upper floor of the admin office")
-    {
-        Console.WriteLine("Congratulations! You won!");
-        Environment.Exit(0);
-    }
-}
+		// check win: gebruik de description van de kamer
+		if (player.CurrentRoom.GetShortDescription() == "in the upper floor of the admin office")
+		{
+			Console.WriteLine("Congratulations! You won!");
+			Environment.Exit(0);
+		}
+	}
 }
